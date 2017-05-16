d6cd042c0104d2f4d66b89dc2b807e03283aab7f
Igor Orekhov <iqor.orekhov@gmail.com>
2017-05-17 00:09:31 +0300

    Players launch order changed

--- src/main/java/com/johnremboo/Main.java
+++ src/main/java/com/johnremboo/Main.java
@@ -6,11 +6,10 @@ package com.johnremboo;
 public class Main {
     public static void main(String[] args) {
         Dispatcher dispatcher = new Dispatcher();
-
         Player player1 = new Player("player1", dispatcher);
-        player1.startConversation("player2", 10);
-
         Player player2 = new Player("player2", dispatcher);
+
+        player1.startConversation("player2", 10);
         player2.startConversation("player1", 10);
     }
 }
